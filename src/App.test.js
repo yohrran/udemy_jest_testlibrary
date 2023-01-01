e6a78ad2bfc665cdf76f7141263c0efb874876b0
yohrran <johnyohan93@gmail.com>
2023-01-01 21:20:20 +0900

    fix: 정규표현식에서 리터럴 형식으로 교체

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getByText("Learn React"); //scren 메소드를 통해 찾는다.
+  expect(linkElement).toBeInTheDocument(); //인자를 안받고, 요가 있는지 없는지 확인한다.
 });
