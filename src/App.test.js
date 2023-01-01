5a98209f3170aff360a94ac99fb82b6236e0b272
yohrran <johnyohan93@gmail.com>
2023-01-01 21:47:02 +0900

    fix: getbytext > getbyRole

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText("Learn React"); //scren 메소드를 통해 찾는다.
+  const linkElement = screen.getByRole("link", { name: /learn react/i }); //scren 메소드를 통해 찾는다.
   expect(linkElement).toBeInTheDocument(); //인자를 안받고, 요가 있는지 없는지 확인한다.
 });
