11e1ec6fff055fffce3a7d5358c64cd804a95e50
yohrran <johnyohan93@gmail.com>
2023-01-05 22:54:23 +0900

    add: eslint, prettier

--- src/App.test.js
+++ src/App.test.js
@@ -24,7 +24,7 @@ test("initial conditions", () => {
 
   //check that the button starts out enabled
   const colorButton = screen.getByRole("button", { name: "Change to blue" });
-  expect(colorButton).toBeEnabled();
+  expect(colorButton).toBeDisabled();
 
   //check that the checkbox starts out unchecked
   const checkbox = screen.getByRole("checkbox");
