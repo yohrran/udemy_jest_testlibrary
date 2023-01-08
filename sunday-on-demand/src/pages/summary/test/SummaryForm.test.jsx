b9df68d260f8ef51dd2ea0f7c0725342bd84554f
yohrran <johnyohan93@gmail.com>
2023-01-08 21:16:57 +0900

    fix: SummaryOrder test changed userEvent.click

--- sunday-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sunday-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,5 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
 test("체크박스가 클릭되어 있지 않으면, 버튼은 활성화 되지 않는다", () => {
@@ -11,13 +12,13 @@ test("체크박스가 클릭되어 있지 않으면, 버튼은 활성화 되지 
   expect(submitButton).toBeDisabled();
 });
 
-test("체크박스가 활성화 시, submit버튼을 클릭할 수 있다", () => {
+test("체크박스가 활성화 시, submit버튼을 클릭할 수 있다", async () => {
   render(<SummaryForm />);
-
+  const user = userEvent.setup();
   const checkbox = screen.getByRole("checkbox", { name: /Terms/i });
   const submitButton = screen.getByRole("button", { name: "submitButton" });
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
 
   expect(checkbox).toBeChecked();
   expect(submitButton).toBeEnabled();
