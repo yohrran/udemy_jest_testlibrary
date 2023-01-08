ab7d4bf173a353781d325b36a363a0615fc75415
yohrran <johnyohan93@gmail.com>
2023-01-08 21:44:17 +0900

    add: Popover, OverlayTrigger

--- sunday-on-demand/src/pages/summary/SummaryForm.jsx
+++ sunday-on-demand/src/pages/summary/SummaryForm.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
 
 export default function SummaryForm() {
   const [isChecked, setIsChecked] = useState(false);
 
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>no ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
+
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: "blue" }}>Terms and Conditions</span>
+      I agree to
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}>Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
+
   return (
     <Form>
       <Form.Group controlId="terms-and-conditions">
