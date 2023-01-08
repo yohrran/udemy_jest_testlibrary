b99ec561e522be245119623f8820e8c3a08c436d
yohrran <johnyohan93@gmail.com>
2023-01-08 21:00:49 +0900

    fix: checkbox name, SummaryForm.jsx component

--- sunday-on-demand/src/pages/summary/SummaryForm.jsx
+++ sunday-on-demand/src/pages/summary/SummaryForm.jsx
@@ -1,3 +1,28 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
 export default function SummaryForm() {
-  return <div />;
+  const [isChecked, setIsChecked] = useState(false);
+
+  const checkboxLabel = (
+    <span>
+      I agree to <span style={{ color: "blue" }}>Terms and Conditions</span>
+    </span>
+  );
+  return (
+    <Form>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={isChecked}
+          onChange={(e) => setIsChecked(e.target.checked)}
+          label={checkboxLabel}
+        />
+        <Button variant="primary" type="submit" disabled={!isChecked}>
+          submitButton
+        </Button>
+      </Form.Group>
+    </Form>
+  );
 }
--- sunday-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sunday-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -4,11 +4,18 @@ import SummaryForm from "../SummaryForm";
 test("체크박스가 클릭되어 있지 않으면, 버튼은 활성화 되지 않는다", () => {
   render(<SummaryForm />);
 
-  const checkbox = screen.getByRole("checkbox", { name: "checkbox" });
+  const checkbox = screen.getByRole("checkbox", { name: /Terms/i });
   expect(checkbox).not.toBeChecked();
 
   const submitButton = screen.getByRole("button", { name: "submitButton" });
   expect(submitButton).toBeDisabled();
+});
+
+test("체크박스가 활성화 시, submit버튼을 클릭할 수 있다", () => {
+  render(<SummaryForm />);
+
+  const checkbox = screen.getByRole("checkbox", { name: /Terms/i });
+  const submitButton = screen.getByRole("button", { name: "submitButton" });
 
   fireEvent.click(checkbox);
 
