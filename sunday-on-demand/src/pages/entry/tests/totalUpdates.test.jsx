0b34fd89d1a24b97dba19aa64e6df2f3ff2f7f56
yohrran <johnyohan93@gmail.com>
2023-01-19 06:04:40 +0900

    fix: test act remove

--- sunday-on-demand/src/pages/entry/tests/totalUpdates.test.jsx
+++ sunday-on-demand/src/pages/entry/tests/totalUpdates.test.jsx
@@ -65,13 +65,6 @@ test("update toppings subtotal when toppings change", async () => {
 });
 
 describe("grand total", () => {
-  test("grand total starts at $0.00", () => {
-    render(<OrderEntry />);
-
-    const grandTotal = screen.getByRole("heading", { name: /Grand total: \$/ });
-    expect(grandTotal).toHaveTextContent("0.00");
-  });
-
   test("grand total updates properly if scoop is added first", async () => {
     const user = userEvent.setup();
     render(<OrderEntry />);
