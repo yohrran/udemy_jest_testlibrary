7c60c8592d301848c7d848ba5222edca11fa15db
yohrran <johnyohan93@gmail.com>
2023-01-20 20:24:44 +0900

    fix: Add switch

--- sunday-on-demand/src/App.js
+++ sunday-on-demand/src/App.js
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import { Container } from "react-bootstrap";
+import OrderConfirmation from "./pages/confirmation/OrderConfirmation";
 import { OrderDetailsProvider } from "./contexts/OrderDetail";
 import OrderEntry from "./pages/entry/OrderEntry";
+import OrderSummary from "./pages/summary/OrderSummary";
+
 function App() {
+  const [orderPhase, setOrderPhase] = useState("inProgress");
+  let Component = OrderEntry;
+
+  switch (orderPhase) {
+    case "inProgress":
+      Component = OrderEntry;
+      break;
+    case "review":
+      Component = OrderSummary;
+      break;
+    case "completed":
+      // eslint-disable-next-line no-unused-vars
+      Component = OrderConfirmation;
+      break;
+    default:
+  }
   return (
     <Container>
       <OrderDetailsProvider>
-        <OrderEntry />
+        <Container>{<Component setOrderPhase={setOrderPhase} />}</Container>
       </OrderDetailsProvider>
     </Container>
   );
